501597383def0f132d3d285debead41d4f6a7b25
John Cruikshank <cruikshj@live.com>
2015-12-03 14:13:26 -0600

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "Skipping release because this is a pull request"
+  exit 0
+fi
+
+echo "Releasing $RELEASE_ENV"
+
+mkdir release
+cp index.html ./release
+
+cd release
+
+git init
+git config user.name "CI"
+git config user.email "CI@github.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/Zywave/Retrospectre.git"
+git fetch upstream
+git reset upstream/release-$RELEASE_ENV
+
+touch .
+
+git add -A .
+git commit -m "CI"
+git push -q upstream HEAD:release-$RELEASE_ENV
