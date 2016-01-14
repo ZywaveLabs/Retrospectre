e2e4a201f084714fd9092b91aa6f2473063bb8d0
TreJon House <houset@msoe.edu>
2016-01-14 15:15:40 -0600

    setup db schema

---
+++ src/js/roomDB.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+*@author TreJon House
+*@date 1/14/16
+*@version 1.0
+*@edited 1/14/16
+*@purpose Display room
+**/
+var lastRoom;
+var ROOMS = new Mongo.Collection("rooms");
+if(Meteor.isClient)
+{
+	Template.createRoom.events({
+		"hover h1": function(event){
+			alert("The last room is "+lastRoom);
+		}
+	});
+}
+if(Meteor.isServer)
+{
+	Meteor.startup(function (){
+		var roomList = ROOMS.find({},{sort: {id: -1}}).fetch();
+		lastRoom = roomList[0].id;
+	});
+}
