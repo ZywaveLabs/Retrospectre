597741d0da37a7139de3031869b958cf30e06e43
CI <CI@github.com>
2016-01-14 19:48:28 +0000

    CI

--- js/retroRoutes.js
+++ js/retroRoutes.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
 *@author TreJon House
 *@created 1/13/16
@@ -5,21 +6,21 @@
 *@edited 1/13/16
 *@purose To define routes for meteor to execute when a link is clicked
 **/
-//configure a template that will used a layout
+// Configure a template that will used a layout
 Router.configure({
-	layoutTemplate: 'headerFooter'//name of template that contains header and footer used for each page
+    layoutTemplate: "headerFooter" // Name of template that contains header and footer used for each page
 });
-//create a route for when directed to the top level homepage
-Router.route('/',{
-	name: 'Home',//give the route a name
-	template:'landingPage',//name of template to render
-	title:'Home'//title of template *for later use*
+// Create a route for when directed to the top level homepage
+Router.route("/",{
+    name: "Home", // Give the route a name
+    template:"landingPage", // Name of template to render
+    title:"Home" // Title of template *for later use*
 });
-//if current session is on the client side then return the title of the current route taken
+// If current session is on the client side then return the title of the current route taken
 if(Meteor.isClient){
-Template.headerFooter.helpers({
-  title:function(){
-	return Router.current().route.options.title;
-  }
-});
+    Template.headerFooter.helpers({
+        title:function(){
+            return Router.current().route.options.title;
+        }
+    });
 }
