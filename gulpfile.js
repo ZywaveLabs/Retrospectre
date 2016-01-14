fffea764e5e397945b5ba39b45c348b36c02b913
Dylan Walseth <DylanWalseth@gmail.com>
2016-01-13 23:24:34 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,6 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
-<<<<<<< HEAD
-      return gulp.src(["src/*.js", "!node_modules/**"])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
-=======
     return gulp.src(["src/*.js", "!node_modules/**"])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
@@ -24,5 +12,4 @@ gulp.task('lint', function() {
     // To have the process exit with an error code (1) on
     // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
->>>>>>> aa2b42075cdfeeab345a122c44aac2fb071062db
 });
