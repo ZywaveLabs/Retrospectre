4ba16884039263f4e237deb8f7d9203be3884557
TreJon House <houset@msoe.edu>
2016-01-18 16:51:21 -0600

    adding implementation to creating a room and joining a room

--- src/js/roomDB.js
+++ src/js/roomDB.js
@@ -7,19 +7,45 @@
 *@purpose Display room
 **/
 var lastRoom;
+var newRoomAssignment;
 var ROOMS = new Mongo.Collection("rooms");
 if(Meteor.isClient)
 {
-	Template.createRoom.events({
-		"hover h1": function(event){
-			alert("The last room is "+lastRoom);
-		}
+	Template.landingPage.events({
+		"click #createRoom": function(event){
+			findLastRoom();
+			if(lastRoom==null || lastRoom==undefined)
+				newRoomAssignment=0;
+			else
+				newRoomAssignment=lastRoom+1;
+			ROOMS.insert({
+				id: newRoomAssignment,
+				dateCreated: new Date()
+			});	
+		},
+		"click #joinRoom": function(event){
+				var roomNumber = prompt('Enter the designated room number.');
+				if(roomNumber != null && isInt(roomNumber))
+					Router.go('/room',{id:roomNumber},{});
+				else
+					alert('An invalid room number was given, either ask for the number or create a new room.');
+					
+			}
 	});
 }
 if(Meteor.isServer)
 {
-	Meteor.startup(function (){
+	Meteor.startup(findLastRoom);
+}
+
+function findLastRoom(){	
 		var roomList = ROOMS.find({},{sort: {id: -1}}).fetch();
-		lastRoom = roomList[0].id;
-	});
+		if(roomList.length>0)
+			lastRoom = roomList[0].id;
+		else
+			lastRoom = null;
+}
+
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
 }
