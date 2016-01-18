2299a9b40c59d9792d9632eb4b76736a03ada559
CI <CI@github.com>
2016-01-18 22:53:21 +0000

    CI

--- js/roomDB.js
+++ js/roomDB.js
@@ -7,7 +7,49 @@
 *@purpose Display room
 **/
 var lastRoom;
+var newRoomAssignment;
 var ROOMS = new Mongo.Collection("rooms");
+<<<<<<< HEAD
+if(Meteor.isClient)
+{
+	Template.landingPage.events({
+		"click #createRoom": function(event){
+			findLastRoom();
+			if(lastRoom==null || lastRoom==undefined)
+				newRoomAssignment=0;
+			else
+				newRoomAssignment=lastRoom+1;
+			ROOMS.insert({
+				id: newRoomAssignment,
+				dateCreated: new Date()
+			});	
+		},
+		"click #joinRoom": function(event){
+				var roomNumber = prompt('Enter the designated room number.');
+				if(roomNumber != null && isInt(roomNumber))
+					Router.go('/room',{id:roomNumber},{});
+				else
+					alert('An invalid room number was given, either ask for the number or create a new room.');
+					
+			}
+	});
+}
+if(Meteor.isServer)
+{
+	Meteor.startup(findLastRoom);
+}
+
+function findLastRoom(){	
+		var roomList = ROOMS.find({},{sort: {id: -1}}).fetch();
+		if(roomList.length>0)
+			lastRoom = roomList[0].id;
+		else
+			lastRoom = null;
+}
+
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+=======
 
 if(Meteor.isClient) {
     Template.createRoom.events({
@@ -22,4 +64,5 @@ if(Meteor.isServer) {
 
         lastRoom = roomList[0].id;
     });
+>>>>>>> 240076700d5381cfdc99f7d0021440aec739a28f
 }
