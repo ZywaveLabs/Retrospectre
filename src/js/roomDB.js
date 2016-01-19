31a81b18a57469f111d7e53637366cda987389fa
TreJon House <houset@msoe.edu>
2016-01-18 18:50:41 -0600

    worked on creating room js and joining room client/server side js

--- src/js/roomDB.js
+++ src/js/roomDB.js
@@ -3,8 +3,8 @@
 *@author TreJon House
 *@date 1/14/16
 *@version 1.0
-*@edited 1/14/16
-*@purpose Display room
+*@edited 1/18/16
+*@purpose find last room added to server and route to desired room if available
 **/
 var lastRoom;
 var newRoomAssignment;
@@ -25,8 +25,9 @@ if(Meteor.isClient)
 		},
 		"click #joinRoom": function(event){
 				var roomNumber = prompt('Enter the designated room number.');
-				if(roomNumber != null && isInt(roomNumber))
-					Router.go('/room',{id:roomNumber},{});
+				roomNumber = parseInt(roomNumber);
+				if(roomNumber != null && !NaN)
+					Router.go('/room',{_id:roomNumber},{});
 				else
 					alert('An invalid room number was given, either ask for the number or create a new room.');
 					
@@ -46,6 +47,3 @@ function findLastRoom(){
 			lastRoom = null;
 }
 
-function isInt(n){
-    return Number(n) === n && n % 1 === 0;
-}
