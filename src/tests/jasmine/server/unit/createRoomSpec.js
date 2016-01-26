f48b5dc19d157a9d5f40e22e6d0c956d58e12821
Dylan Walseth <DylanWalseth@gmail.com>
2016-01-26 14:22:42 -0600

    Testing framework setup... good luck figuring out

---
+++ src/tests/jasmine/server/unit/createRoomSpec.js
@@ -0,0 +1,6 @@
+"use strict";
+describe("CreateRoom", function() {
+    // recommended against using unit testing in server
+    // we will probably do it anyways
+    // https://meteor-testing.readme.io/v1.0/docs/jasmine-testing-modes
+});
