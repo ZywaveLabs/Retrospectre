8b47c131d27f055508dbe2ba3a139f133209cb68
Dylan Walseth <DylanWalseth@gmail.com>
2016-01-25 21:27:59 -0600

    Testing demeteorizer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+/*eslint-disable */
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+var shell = require("gulp-shell");
 
-gulp.task('lint', function() {
-    return gulp.src(["src/**/*.js", "!node_modules/**", "!src/public/js/bootstrap.js", "!src/public/js/bootstrap.min.js", "!src/public/js/npm.js"])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
-    .pipe(eslint.failAfterError());
+gulp.task("lint", function() {
+    return gulp.src(["src/**/*.js"])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+
+gulp.task("bash", function() {
+    return gulp.src("*.js", {read: false})
+    .pipe(shell([
+        "echo \"This task is not setup yet, go away\"",
+    ]));
 });
