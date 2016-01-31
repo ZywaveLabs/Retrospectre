ca09e0c20a0ad1b4767c3b00ce19b18f19bbf5d8
Dylan Walseth <dylanwalseth@gmail.com>
2016-01-31 13:03:23 -0600

    Undo breaking tests

--- src/tests/jasmine/client/unit/sample/spec/PlayerSpec.js
+++ src/tests/jasmine/client/unit/sample/spec/PlayerSpec.js
@@ -11,7 +11,7 @@ describe('Player', function() {
 
   it('should be able to play a Song', function() {
     player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(0);
+    expect(player.currentlyPlayingSong).toEqual(song);
 
     //demonstrates use of custom matcher
     expect(player).toBePlaying(song);
