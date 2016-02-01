ef13b9d4796728b089f65fa634e78f22c95236a5
Dylan Walseth <DylanWalseth@gmail.com>
2016-01-31 18:48:34 -0600

    Rebase

--- src/js/deleteCard.js
+++ src/js/deleteCard.js
@@ -1,6 +1,7 @@
 "use strict";
 
 /**
+<<<<<<< 41040f6b048d16203907168769ace34a71a8d1c4
  *@author Dustin Chiasson
  *@purpose To provide functionality for removing a card from the room
  **/
@@ -15,3 +16,19 @@ Meteor.methods({
         });
     }
 });
+=======
+*@author Dustin Chiasson
+*@purpose To provide functionality for removing a card from the room
+**/
+
+Meteor.methods({
+  deleteCard: function (roomNumber, category, text) {
+    Mongo.Collection.get("cards").remove({
+      roomCode: roomNumber,
+      category: category,
+      createdAt: new Date(),
+      text: text
+    });
+  }
+});
+>>>>>>> Created js file for deleteCard
