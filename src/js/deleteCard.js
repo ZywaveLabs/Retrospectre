e580090a3ad8e9645e138afc420b5318525a0529
Dustin Chiasson <chiassond@msoe.edu>
2016-01-31 18:10:41 -0600

    Created js file for deleteCard

---
+++ src/js/deleteCard.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+*@author Dustin Chiasson
+*@purpose To provide functionality for removing a card from the room
+**/
+
+Meteor.methods({
+  deleteCard: function (roomNumber, category, text) {
+    Mongo.Collection.get("cards").remove({
+      roomCode: roomNumber,
+      category: category,
+      createdAt: new Date(),
+      text: text
+    });
+  }
+});
