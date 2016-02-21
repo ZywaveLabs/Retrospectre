b1c4dc9b90e71a9ae450453db3ab3f617faed414
Dylan Walseth <DylanWalseth@gmail.com>
2016-02-20 20:17:05 -0600

    Test fixes

--- src/tests/jasmine/client/unit/createCard/createCardWithTagTest.js
+++ src/tests/jasmine/client/unit/createCard/createCardWithTagTest.js
@@ -1,116 +1,158 @@
-/*eslint-disable*/
-describe("add valid cards with tags",function(){
+// /*eslint-disable*/
+describe("add valid cards with tags", function() {
 
-  beforeEach(function() {
-    spyOn(Meteor, "call").and.callThrough();
-    spyOn(Cards, "insert");
-  });
+    beforeEach(function() {
+        spyOn(Meteor, "call").and.callThrough();
+        spyOn(Cards, "insert");
+    });
 
-  it("should add card with one tag",function(){
-    event = jasmine.createSpyObj("event",["target","preventDefault"]);
-    event = {
-      target: {
-        thoughts : {value: "testThought"},
-        tags: {value: "testTag"},
-        likes: {value:0},
-        author: {value: "testAuthor"},
-        goodCategoryRadio: {checked: true},
-        badCategoryRadio: {checked: false}
-      },
-      preventDefault: function(){}
-    };
-    spyOn(Session, "get").and.returnValue("testRoom");
-    spyOn(Session, "set");
-    var baseTime = new Date();
-    jasmine.clock().mockDate(baseTime);
+    it("should add card with one tag", function() {
+        var eventObj = {
+            event: {
+                target: {
+                    thoughts: {
+                        value: "testThought"
+                    },
+                    tags: {
+                        value: "testTag"
+                    },
+                    likes: {
+                        value: 0
+                    },
+                    author: {
+                        value: "testAuthor"
+                    },
+                    goodCategoryRadio: {
+                        checked: true
+                    },
+                    badCategoryRadio: {
+                        checked: false
+                    }
+                },
+                preventDefault: function() {}
+            }
+        };
+        spyOn(Session, "get").and.returnValue("testRoom");
+        spyOn(Session, "set");
+        var baseTime = new Date();
+        jasmine.clock().mockDate(baseTime);
 
-    Template.card.fireEvent("submit #card");
-    expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
-              "good", "testThought",["testTag"], 0,"testAuthor"]);
-    expect(Cards.insert).toHaveBeenCalledWith({
-          roomCode: "testRoom",
-          category: "good",
-          createdAt: baseTime,
-          text: "testThought",
-          tags: ["testTag"],
-          likes: 0,
-          author: "testAuthor",
-          reveal: false
-      });
-    expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
-    expect(event.target.thoughts.value).toEqual("");
-    expect(event.target.tags.value).toEqual("");
-  });
+        Template.card.fireEvent("submit #card", eventObj);
+        expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
+            "good", "testThought", ["testTag"], 0, "testAuthor"
+        ]);
+        expect(Cards.insert).toHaveBeenCalledWith({
+            roomCode: "testRoom",
+            category: "good",
+            createdAt: baseTime,
+            text: "testThought",
+            tags: ["testTag"],
+            likes: 0,
+            author: "testAuthor",
+            reveal: false
+        });
+        expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
+        expect(eventObj.event.target.thoughts.value).toEqual("");
+        expect(eventObj.event.target.tags.value).toEqual("");
+    });
 
-  it("should add card with multiple tags",function(){
-    event = jasmine.createSpyObj("event",["target","preventDefault"]);
-    event = {
-      target: {
-        thoughts : {value: "testThought"},
-        tags: {value: "testTag,jasmine unit,testing"},
-        likes: {value:0},
-        author: {value: "testAuthor"},
-        goodCategoryRadio: {checked: true},
-        badCategoryRadio: {checked: false}
-      },
-      preventDefault: function(){}
-    };
-    spyOn(Session, "get").and.returnValue("testRoom");
-    spyOn(Session, "set");
-    var baseTime = new Date();
-    jasmine.clock().mockDate(baseTime);
+    it("should add card with multiple tags", function() {
+        var eventObj = {
+            event: {
+                target: {
+                    thoughts: {
+                        value: "testThought"
+                    },
+                    tags: {
+                        value: "testTag,jasmine unit,testing"
+                    },
+                    likes: {
+                        value: 0
+                    },
+                    author: {
+                        value: "testAuthor"
+                    },
+                    goodCategoryRadio: {
+                        checked: true
+                    },
+                    badCategoryRadio: {
+                        checked: false
+                    }
+                },
+                preventDefault: function() {}
+            }
+        };
+        spyOn(Session, "get").and.returnValue("testRoom");
+        spyOn(Session, "set");
+        var baseTime = new Date();
+        jasmine.clock().mockDate(baseTime);
 
-    Template.card.fireEvent("submit #card");
-    expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
-              "good", "testThought",["testTag","jasmine unit","testing"], 0,"testAuthor"]);
-    expect(Cards.insert).toHaveBeenCalledWith({
-          roomCode: "testRoom",
-          category: "good",
-          createdAt: baseTime,
-          text: "testThought",
-          tags: ["testTag","jasmine unit","testing"],
-          likes:0,
-          author: "testAuthor",
-          reveal: false
-      });
-    expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
-    expect(event.target.thoughts.value).toEqual("");
-    expect(event.target.tags.value).toEqual("");
-  });
+        Template.card.fireEvent("submit #card", eventObj);
+        expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
+            "good", "testThought", ["testTag", "jasmine unit", "testing"], 0, "testAuthor"
+        ]);
+        expect(Cards.insert).toHaveBeenCalledWith({
+            roomCode: "testRoom",
+            category: "good",
+            createdAt: baseTime,
+            text: "testThought",
+            tags: ["testTag", "jasmine unit", "testing"],
+            likes: 0,
+            author: "testAuthor",
+            reveal: false
+        });
+        expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
+        expect(eventObj.event.target.thoughts.value).toEqual("");
+        expect(eventObj.event.target.tags.value).toEqual("");
+    });
 
-  it("should add card with one tag, though given several of same tag",function(){
-    event = jasmine.createSpyObj("event",["target","preventDefault"]);
-    event = {
-      target: {
-        thoughts : {value: "testThought"},
-        tags: {value: "testTag,testTag,testTag,testTag"},
-        likes: {value:0},
-        author: {value: "testAuthor"},
-        goodCategoryRadio: {checked: true},
-        badCategoryRadio: {checked: false}
-      },
-      preventDefault: function(){}
-    };
-    spyOn(Session, "get").and.returnValue("testRoom");
-    spyOn(Session, "set");
-    var baseTime = new Date();
-    jasmine.clock().mockDate(baseTime);
+    it("should add card with one tag, though given several of same tag", function() {
+        var eventObj = {
+            event: {
+                target: {
+                    thoughts: {
+                        value: "testThought"
+                    },
+                    tags: {
+                        value: "testTag,testTag,testTag,testTag"
+                    },
+                    likes: {
+                        value: 0
+                    },
+                    author: {
+                        value: "testAuthor"
+                    },
+                    goodCategoryRadio: {
+                        checked: true
+                    },
+                    badCategoryRadio: {
+                        checked: false
+                    }
+                },
+                preventDefault: function() {}
+            }
+        };
+        spyOn(Session, "get").and.returnValue("testRoom");
+        spyOn(Session, "set");
+        var baseTime = new Date();
+        jasmine.clock().mockDate(baseTime);
 
-    Template.card.fireEvent("submit #card");
-    expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
-              "good", "testThought",["testTag"],0, "testAuthor"]);
-    expect(Cards.insert).toHaveBeenCalledWith({
-          roomCode: "testRoom",
-          category: "good",
-          createdAt: baseTime,
-          text: "testThought",
-          tags: ["testTag"],
-          likes: 0,
-          author: "testAuthor",
-          reveal: false
-      });
-    expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
-    expect(event.target.thoughts.value).toEqual("");
-    expect(event.target.tags.value).toEqual("");
-  });
+        Template.card.fireEvent("submit #card", eventObj);
+        expect(Meteor.call).toHaveBeenCalledWith("submitCardWithTags", ["testRoom",
+            "good", "testThought", ["testTag"], 0, "testAuthor"
+        ]);
+        expect(Cards.insert).toHaveBeenCalledWith({
+            roomCode: "testRoom",
+            category: "good",
+            createdAt: baseTime,
+            text: "testThought",
+            tags: ["testTag"],
+            likes: 0,
+            author: "testAuthor",
+            reveal: false
+        });
+        expect(Session.set).toHaveBeenCalledWith("author", "testAuthor");
+        expect(eventObj.event.target.thoughts.value).toEqual("");
+        expect(eventObj.event.target.tags.value).toEqual("");
+    });
 });
