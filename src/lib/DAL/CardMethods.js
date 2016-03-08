ffb9d5a5dd7080c8f6eba5ba1da2f860e34baeb9
Dylan Walseth <dylanwalseth@gmail.com>
2016-03-07 21:16:45 -0600

    Some small changes for funsies

--- src/lib/DAL/CardMethods.js
+++ src/lib/DAL/CardMethods.js
@@ -1,5 +1,5 @@
 // /* eslint-disable */
-/* global Cards:true CardMethods:true ProgramUtils:true */
+/* global Cards:true CardMethods:true ProgramUtils:true Card:true */
 
 Cards = new Mongo.Collection("cards");
 if(Meteor.isServer){
@@ -23,6 +23,49 @@ author - {string} Author of the card
 reveal - {boolean} If the card should be visible to everyone besides author
 comments - {object[]} Comments added to card after creation as notes
 */
+Card = function(){};
+Card.prototype.inRoom = function (roomCode) {
+    this.roomCode = roomCode;
+    return this;
+};
+Card.prototype.withCategory = function (category) {
+    this.category = category;
+    return this;
+};
+Card.prototype.createdAt = function (dateTime) {
+    this.createdAt = dateTime;
+    return this;
+};
+Card.prototype.withText = function (text) {
+    this.text = text;
+    return this;
+};
+Card.prototype.withTags = function (tags) {
+    this.tags = tags;
+    return this;
+};
+Card.prototype.havingLikes = function (likes) {
+    this.likes = likes;
+    return this;
+};
+Card.prototype.createdBy = function (author) {
+    this.author = author;
+    return this;
+};
+Card.prototype.shouldBeRevealed = function (reveal) {
+    this.reveal = reveal;
+    return this;
+};
+Card.prototype.createdBy = function (comments) {
+    this.comments = comments;
+    return this;
+};
+
+var myCard = new Card()
+                .inRoom("Test")
+                .withCategory("Went Well")
+                .withText("Test");
+
 
 /*
 CommentObject
