ae25b4cafb87ec1dc16ffa41984c230fabdbef26
TreJon House <houset@msoe.edu>
2016-03-09 16:24:14 -0600

    added images to put on new card design, added css styling and js to card design as well

--- src/client/scripts/card.js
+++ src/client/scripts/card.js
@@ -4,5 +4,15 @@
 Template.card.helpers({
     cardInfo: function(_id) {
         return Cards.findOne({"_id": _id});
+    },
+
+    randomCardImage: function(){
+        var imageArray = ["cloud.gif","aeris.png","barret.png","sephiroth.png",
+        "tifa.png","vincent.png","yuffie.png","spectre.png"];
+        var randImage;
+
+        randImage = "/" +
+          imageArray[Math.floor(Math.random() * imageArray.length)];
+        return randImage;
     }
 });
