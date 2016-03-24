043fef7731d9038a153b8c642c977413895f200c
TreJon House <houset@msoe.edu>
2016-03-24 18:41:46 -0500

    got rid of junk methods i added

--- src/client/scripts/sharejsEditor.js
+++ src/client/scripts/sharejsEditor.js
@@ -5,18 +5,6 @@ Template.room.helpers({
         return function(editor) {
             editor.setTheme("ace/theme/pastel_on_dark");
             editor.setShowPrintMargin(false);
-            var prevText;
-
-            prevText = RoomMethods.getKeynotes(Session.get("roomNumber"));
-            if (prevText && prevText.length != 0) {
-                prevText = prevText.split("\n");
-                var prevSavedText = "";
-
-                for (var i = 0; i < prevText.length; i++) {
-                    prevSavedText += prevText[i] + "\n";
-                }
-                editor.setValue(prevSavedText);
-            }
         };
     },
 
