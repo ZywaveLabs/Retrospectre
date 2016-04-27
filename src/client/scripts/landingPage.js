8f22eb8ffa6f93f411cd92df3412c3dd183a2cbc
Jacob Huemann <jacobhuemann@MacBook-Pro.nebula.msoe.edu>
2016-04-27 10:17:18 -0500

    linting

--- src/client/scripts/landingPage.js
+++ src/client/scripts/landingPage.js
@@ -1,12 +1,12 @@
 "use strict";
-/* globals SnackbarMethods:false */
+/* globals DEFAULT_SNACKBAR_TIMEOUT:true SnackbarMethods:false */
 
 Template.landingPage.events({
 
     "click #createRoomButton": function () {
-        if(!Meteor.user())
-            SnackbarMethods.DisplayMessage("Warning: Log" +
-                " in via Google before creating room",3000);
+        if(!Meteor.user()) {
+            SnackbarMethods.DisplayMessage("Warning: Log in via Google before creating room",DEFAULT_SNACKBAR_TIMEOUT);
+        }
         Router.go("Create Room");
     }
 });
