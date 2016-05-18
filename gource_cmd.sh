3d028720af7771759a6a4db139dbd5b4e7cb3ed6
Dylan Walseth <dylanwalseth@gmail.com>
2016-05-17 23:12:10 -0500

    Added hide all cards button so rooms can be reused, quick update before summer, also made the gource cmd a little faster to run

--- gource_cmd.sh
+++ gource_cmd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gource -f --start-date '2016-02-24 23:59:59 -6' -c 2.0 --colour-images --title 'Retrospectre Spring Quarter 2016' -a 0.5 --user-friction .2 --hide mouse --key
+gource -f --start-date '2016-02-24 23:59:59 -6' -c 3.0 --colour-images --title 'Retrospectre Spring Quarter 2016' -a 0.4 --user-friction .2 --key
